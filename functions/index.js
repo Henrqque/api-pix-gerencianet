d7b152c72fd53256f46978cbd2f320984d8386ac
Henriqque <hdoido1992@gmail.com>
2022-10-23 01:25:25 -0300

    Refatorando código

--- functions/index.js
+++ functions/index.js
@@ -3,52 +3,14 @@ if(process.env.NODE_ENV !== 'production') {
 }
 
 const express = require('express');
-const axios = require('axios');
-const fs = require('fs');
-const path = require('path');
-const https = require('https');
+const GNRequest = require('./src/apis/gerencianet');
 
 const app = express();
+const reqGNAlready = GNRequest();
 
-// pegando o certificado //retorna em buffer
-const cert = fs.readFileSync(
-    path.resolve(__dirname, `./certs/${process.env.GN_CERT}`)
-);
-
-const agent = new https.Agent({
-    pfx: cert,
-    passphrase: ''
-});
-
-//token
-const credentials = Buffer.from(
-    `${process.env.GN_CLIENT_ID}:${process.env.GN_CLIENT_SECRET}`
-).toString('base64');
 
 app.get('/', async(req, res) => {
-    const authResposne = await axios({
-        method: 'POST',
-        url: `${process.env.GN_ENDPOINT}/oauth/token`,
-        headers:{
-            Authorization: `Basic ${credentials}`,
-            'Content-Type': 'application/json'
-        },
-        httpsAgent: agent,
-        data: {
-            grant_type: 'client_credentials'
-        }
-    })
-    const acessToken = authResposne.data?.access_token;
-
-    const reqGN = axios.create({
-        baseURL: process.env.GN_ENDPOINT,
-        httpsAgent: agent,
-        headers:{
-            Authorization: `Bearer ${acessToken}`,
-            'Content-Type': 'application/json'
-        }
-    })
-
+    const reqGN = await reqGNAlready;
     const dataCob = {
         calendario: {
             expiracao: 3600
@@ -60,16 +22,8 @@ app.get('/', async(req, res) => {
         solicitacaoPagador: "Cobrança dos serviços prestado"
     }
 
-    const config = {
-        httpsAgent: agent,
-        headers:{
-            Authorization: `Bearer ${acessToken}`,
-            'Content-Type': 'application/json'
-        }
-    };
-
-    const cobResponse = await reqGN.post('v2/cob', dataCob, config);
-    const qrcodeResponse = await reqGN.get(`/v2/loc/${cobResponse.data.loc.id}/qrcode`, dataCob, config);
+    const cobResponse = await reqGN.post('v2/cob', dataCob);
+    const qrcodeResponse = await reqGN.get(`/v2/loc/${cobResponse.data.loc.id}/qrcode`, dataCob);
 
     res.send(qrcodeResponse.data)
 })
